62e090d439a3f2423344c5132d3e4e1e439007c2
Yellowpig-zp <zipeng.qin.1997@outlook.com>
2018-07-17 21:41:44 -0700

    fixed typo

--- settings.py
+++ settings.py
@@ -1,3 +1,3 @@
 IMAGE_PATH = "/home/qinzp97/local/hdd1/picked_dataset/edgemaps/"
 
-SAVED_PATH = "/home/qinzp97/local/hdd1/picked_dataset/edgamaps_resized/"
+SAVED_PATH = "/home/qinzp97/local/hdd1/picked_dataset/edgemaps_resized/"
